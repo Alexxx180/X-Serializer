c9b7554905a7ab37edf624e178fdb72505f4251d
Alexxx180 <alexx120702@gmail.com>
2022-05-29 21:17:13 +0300

    Bit array to byte array converter

---
+++ Converters/Json/BitArrayConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Processors.Converters.Json
+{
+    public class BitArrayConverter : JsonConverter<BitArray>
+    {
+        class BitArrayDTO
+        {
+            public byte[] Bytes { get; set; }
+            public int Length { get; set; }
+        }
+
+        public override BitArray Read(ref Utf8JsonReader reader, Type objectType, JsonSerializerOptions options)
+        {
+            BitArrayDTO dto = JsonSerializer.Deserialize<BitArrayDTO>(ref reader);
+
+            if (dto is null)
+                return null;
+
+            var bitArray = new BitArray(dto.Bytes);
+            bitArray.Length = dto.Length;
+
+            return bitArray;
+        }
+
+        public override void Write(Utf8JsonWriter writer, BitArray value, JsonSerializerOptions options)
+        {
+            byte[] serializable = new byte[(int)Math.Ceiling(value.Length / 8.0)];
+            value.CopyTo(serializable, 0);
+
+            BitArrayDTO dto = new BitArrayDTO
+            {
+                Bytes = serializable,
+                Length = value.Length
+            };
+            
+            JsonSerializer.Serialize(writer, dto);
+        }
+    }
+}
--- Json.cs
+++ Json.cs
@@ -5,17 +5,29 @@ namespace Processors
 {
     public class Json : Serializer
     {
+        public Json()
+        {
+            _options = new JsonSerializerOptions();
+        }
+
+        public Json(JsonSerializerOptions options)
+        {
+            _options = options;
+        }
+
         private protected override T Deserialize<T>(string path)
         {
             byte[] fileBytes = File.ReadAllBytes(path);
             Utf8JsonReader utf8Reader = new Utf8JsonReader(fileBytes);
-            return JsonSerializer.Deserialize<T>(ref utf8Reader);
+            return JsonSerializer.Deserialize<T>(ref utf8Reader, _options);
         }
 
         private protected override void Serialize<T>(T serializable, string path)
         {
-            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(serializable);
+            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(serializable, _options);
             File.WriteAllBytes(path, jsonUtf8Bytes);
         }
+
+        private JsonSerializerOptions _options;
     }
 }
