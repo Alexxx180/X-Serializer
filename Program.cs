ec64aa7143ec72fd6a424d74bafaaf4d11ba8d7d
Alexxx180 <alexx120702@gmail.com>
2022-05-29 10:02:14 +0300

    Console project to test serializer api

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Processors;
+
+namespace XSerializer
+{
+    internal class Program
+    {
+        static Program()
+        {
+            _serializer = new Json();
+        }
+
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to end.");
+            Console.ReadKey();
+        }
+
+        private static Serializer _serializer;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# X-Serializer
+
+Test serializer capabilities by hand
